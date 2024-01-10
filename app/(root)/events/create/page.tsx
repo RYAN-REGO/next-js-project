7ec7555de1dd0bb78e66d0fa00157df6b30ef16a
Ryan <ryan.work321@gmail.com>
2024-01-10 20:21:15 +0530

    Event form ui started

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,24 @@
+import EventForm from "@/components/shared/EventForm";
+import { auth } from "@clerk/nextjs";
+
+const CreateEvent = () => {
+    const { sessionClaims } = auth();
+
+    const userId = sessionClaims?.userId as string;
+
+  return(
+    <>
+        <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+            <h3 className="wrapper h3-bold text-center sm:text-left">Create Event</h3>
+        </section>
+        <div className="wrapper my-8">
+            {/* //passing the user id of the user interacting with the form */}
+            <EventForm userId = {userId} type="Create"/>
+        </div>
+    </>
+  )
+
+  //the same EventForm is used to create as well as update the event section
+};
+
+export default CreateEvent;
